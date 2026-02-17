90c9310379f58917870d28585c1d48f28184fd83
Klein Lavina <kleinlav7@gmail.com>
2026-02-17 22:23:00 +0800

    Force clear static files cache

--- build.sh
+++ build.sh
@@ -6,7 +6,10 @@ set -o errexit
 pip install -r requirements.txt
 
 # Collect static files
-python manage.py collectstatic --no-input
+python manage.py collectstatic --no-input --clear
 
 # Run migrations
 python manage.py migrate
+
+# Create admin user
+python manage.py create_admin
